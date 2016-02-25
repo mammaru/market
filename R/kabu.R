18789fe955d90c18c9687ef583530105afcda1cb
mammaru <mauma1989@gmail.com>
2016-02-25 13:07:13 +0900

    ignore .RData and .Rhistory

--- R/kabu.R
+++ R/kabu.R
@@ -1,9 +1,11 @@
-#library(quantmod)
+library(quantmod)
 #sony1 <- getSymbols("6758.T", src="yahooj", auto.assign=FALSE)
 #head(sony1)
+chartSeries(sony1, subset="2015::2015-04", theme=chartTheme("white"), TA="addVo(); addBBands()")
+reChart(subset="2015-02-01::2016-02-31")
 
-#chartSeries(sony1, subset="2015::2015-04", theme=chartTheme("white"), TA="addVo(); addBBands()")
-reChart(subset="2016-01-01::2016-02-31")
+
+
 
 #library(RFinanceYJ)
 #sony2 <- quoteStockTsData("6758.T", since="2016-01-01") 
