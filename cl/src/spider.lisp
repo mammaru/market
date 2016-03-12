1dd30f4cc2da3ffde0791ee9f92b96ef921ce736
Yuma M <mauma1989@gmail.com>
2016-03-12 14:54:13 +0900

    add define-spider

--- cl/src/spider.lisp
+++ cl/src/spider.lisp
@@ -4,67 +4,88 @@
 	(:use common-lisp
 				drakma)
 	(:nicknames cl-crwl)
-	(:export spider
-					 parse
+	(:export parse
 					 scrape
+					 define-spider
 					 crawl))
 
 (in-package :crawl)
 
+(defmacro with-gensyms (syms &body body)
+	`(let ,(mapcar #'(lambda (s) `(,s (gensym))) syms)
+		 ,@body))
+
 (defclass spider ()
-	((name
-		:initarg :name
-		:initform (error "Must specify name")
-		:accessor name)
-	 (root-url
-		:initarg :root-url
+	((root-url
 		:initform (error "Must specify root url")
-		:accessor root-url)
-	 (parser
-		:initarg :parser
-		:initform #'(lambda (doc) doc)
-		:accessor parser
-		:type 'lambda)
-	 (recursive?
-		:initarg :recursive?
-		:initform nil
-		:accessor recursive?)
+		:accessor root-url
+		:type 'string)
 	 (sleep-time
-		:initarg :sleep-time
+		:initarg :sleep
 		:initform 5
 		:accessor sleep-time
-		:type 'number)))
+		:type 'number)
+	 (fetched-doc
+		:initform nil
+		:accessor doc
+		:type 'string) ))
 
 (defgeneric fetch (spider url)
 	(:documentation "fetch web by http"))
 
-(defgeneric parse (spider doc)
+(defgeneric parse (spider)
 	(:documentation "parse html"))
 
 (defgeneric scrape (spider)
 	(:documentation "fetch and parse html"))
 
 (defmethod fetch (spider url)
-	(drakma:http-request url :want-stream t))
+	(with-slots ((doc doc)) spider
+		(setf doc (drakma:http-request url)) ))
 
-(defmethod parse (spider doc)
-	(with-slots ((parser parser)) spider
-		(funcall parser doc) ))
+(defmethod parse (spider)
+	(with-slots ((doc doc)) spider
+		(values doc nil) ))
 
 (defmethod scrape (spider)
-	(with-slots ((root root-url) (r-flag recursive?) (sleep-time sleep-time)) spider
-		(if r-flag
-				(let (results)
-					(labels ((recursive-scrape (url)
-										 (multiple-value-bind (result next-url) (parse spider (fetch spider url))
-											 (cons result results)
-											 (if next-url
-													 (progn
-														 (sleep sleep-time)
-														 (recursive-scrape next-url))
-													 results) )))
-						(recursive-scrape root) ))
-				(parse (fetch root)) )))
+	(with-slots ((root root-url) (sleep-time sleep-url)) spider
+		(let (results)
+			(labels ((recursive-scrape (url)
+								 (progn
+									 (fetch spider url)
+									 (multiple-value-bind (result next-url) (parse spider)
+										 (cons result results)
+										 (if next-url
+												 (progn
+													 (sleep sleep-time)
+													 (recursive-scrape next-url))
+												 results) ))))
+				(recursive-scrape root) ))))
+
+(defmacro define-spider (sp-name (document sp-root-url &key sleep) &body body)
+	(with-gensyms (sp)
+		`(progn
+			 (defclass ,sp-name (spider)
+				 ((root-url
+					 :initform ,sp-root-url
+					 :accessor root-url
+					 :type 'string)
+					,(if sleep
+							 `(sleep-time
+								 :initarg :sleep
+								 :initform ,sleep
+								 :accessor sleep-time) )))
+			 (defmethod parse ((,sp ,sp-name))
+				 (with-slots ((,document doc)) ,sp
+					 ,@body) ))))
+
+;;;(defmacro with-scrape 
+;;;									:root-url ,sp-root-url
+;;;									,@(if recursive-p	`(:recursive? ,recursive-p))
+;;;									,@(if sleep-time `(:sleep-time ,sleep-time))
+;;;									,@(if body `(:parser ,@body) ))
+
+
 
 (defclass crawler ()
 	((spider
