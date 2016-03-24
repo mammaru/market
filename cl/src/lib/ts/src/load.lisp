42c73190ac5c2908ef961ca3c8431428507a9137
Yuma M <mauma1989@gmail.com>
2016-03-25 02:45:14 +0900

    move lisp files private repository

--- cl/src/lib/ts/src/load.lisp
+++
@@ -1,5 +0,0 @@
-(load (merge-pathnames "packages.lisp"))
-
-(in-package :clts-user)
-(load (merge-pathnames "utilities.lisp"))
-(load (merge-pathnames "models.lisp"))
--- cl/src/mecab.lisp
+++
@@ -1,6 +0,0 @@
-;;;(eval-when (:compile-toplevel :load-toplevel)
-;;;	(ql:quickload :mecab))
-
-(mecab:sparse-tostr "めかぶは日本語文字列を形態素解析するためのライブラリです。")
-
-(mecab:wakati "めかぶは日本語文字列を形態素解析するためのライブラリです。")
